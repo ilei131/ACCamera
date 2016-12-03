1734bb163b4a1803675c51378a20003660ede378
ilei <383990463@qq.com>
2016-12-03 22:52:08 +0800

    test

---
+++ ACCamera/Vendor/TIMChat/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  ACCamera
+//
+//  Created by Guo Xuelei on 2016/12/3.
+//  Copyright © 2016年 Zhanggf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ ACCamera/Vendor/TIMChat/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  ACCamera
+//
+//  Created by Guo Xuelei on 2016/12/3.
+//  Copyright © 2016年 Zhanggf. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
